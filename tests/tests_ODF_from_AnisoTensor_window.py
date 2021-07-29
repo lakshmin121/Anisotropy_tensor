dc2646c1160a80491e0c11eb38b861f19a882540
Lakshminarayanan Mohana Kumar <l.mohanakumar@unsw.edu.au>
2021-07-30 07:45:08 +1000

    No changes

--- tests/tests_ODF_from_AnisoTensor_window.py
+++ tests/tests_ODF_from_AnisoTensor_window.py
@@ -10,7 +10,6 @@ import sys
 import numpy as np
 import pandas as pd
 sys.path.append("../artificial images/")
-import orientation_probabilities as op
 import skimage.io as skio
 from fiborient import tensor2odf_2D, orient_tensor_2D
 from fibfourier import fourier_orient_tensor, localised_fourier_orient_tensor

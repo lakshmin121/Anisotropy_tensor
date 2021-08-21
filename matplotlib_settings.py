8d00f67106b40f62fe04d573249c3ae74cdb5041
Lakshminarayanan Mohana Kumar <l.mohanakumar@unsw.edu.au>
2021-08-22 08:55:05 +1000

    Updates to legend settings.

--- matplotlib_settings.py
+++ matplotlib_settings.py
@@ -1,8 +1,13 @@
 """
-Settings for preparing consistent and beautiful figures for thesis.
+Matplotlib settings for preparing consistent and beautiful figures for thesis.
+_____________________________________________________________________________
+@ Lakshminarayanan Mohana Kumar
+updated on: 22nd Aug 2021.
 """
 import matplotlib as mpl
 import seaborn as sns
+
+__version__ = '1.1'
 
 # units
 # 1 pt = 1/72 in = 0.353 mm
@@ -62,11 +67,11 @@ mpl.rcParams['axes.labelweight'] = 'bold'
 # TICKS
 mpl.rcParams['xtick.labelsize'] = 10.
 mpl.rcParams['xtick.major.size'] = 5        # major tick size in points
-mpl.rcParams['xtick.minor.size'] = 2.5        # minor tick size in points
-mpl.rcParams['xtick.major.width'] = 0.8       # major tick width in points
+mpl.rcParams['xtick.minor.size'] = 2.5      # minor tick size in points
+mpl.rcParams['xtick.major.width'] = 0.8     # major tick width in points
 mpl.rcParams['xtick.minor.width'] = 0.8     # minor tick width in points
 # mpl.rcParams['xtick.major.pad'] = 3.5     # distance to major tick label in points
-# mpl.rcParams['xtick.minor.pad'] = 3.4     # distan    ce to the minor tick label in points
+# mpl.rcParams['xtick.minor.pad'] = 3.4     # distance to the minor tick label in points
 mpl.rcParams['ytick.labelsize'] = 10.
 mpl.rcParams['ytick.major.size'] = 5        # major tick size in points
 mpl.rcParams['ytick.minor.size'] = 2.5      # minor tick size in points
@@ -79,13 +84,19 @@ mpl.rcParams['ytick.minor.width'] = 0.5     # minor tick width in points
 # mpl.rcParams['grid.alpha'] = 0.5
 
 # LEGEND
-mpl.rcParams['legend.markerscale'] = 1.0
+mpl.rcParams['legend.fancybox'] = False     # if True, use a rounded box for the legend background
+mpl.rcParams['legend.edgecolor'] = '0.2'    # background patch boundary color
+mpl.rcParams['legend.markerscale'] = 1.0    # the relative size of legend markers vs. original
 mpl.rcParams['legend.fontsize'] = 10.0
-mpl.rcParams['legend.borderpad'] = 0.3
-mpl.rcParams['legend.labelspacing'] = 0.5
-mpl.rcParams['legend.handlelength'] = 2.0
-mpl.rcParams['legend.handletextpad'] = 0.6
-mpl.rcParams['legend.columnspacing'] = 0.5
+mpl.rcParams['patch.linewidth'] = 0.5       # legend frame lw. Note: affects other patches*
+# Dimensions as fraction of font size:
+mpl.rcParams['legend.borderpad'] = 0.3      # border whitespace
+mpl.rcParams['legend.labelspacing'] = 0.5   # the vertical space between the legend entries
+mpl.rcParams['legend.handlelength'] = 2.0   # the length of the legend lines
+mpl.rcParams['legend.handleheight'] = 0.7   # the height of the legend handle
+mpl.rcParams['legend.handletextpad'] = 0.6  # the space between the legend line and legend text
+mpl.rcParams['legend.borderaxespad'] = 0.2  # the border between the axes and legend edge
+mpl.rcParams['legend.columnspacing'] = 0.5  # column separation
 
 # FIGURE
 mpl.rcParams['figure.titlesize'] = 12.
